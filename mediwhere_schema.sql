7fcfb21a79cc1312e5c14d75f035a7d478394499
Viteszeg <rofi_santos@dlsu.edu.ph>
2016-08-07 21:36:43 +0800

    added data and changed names

--- mediwhere_schema.sql
+++ mediwhere_schema.sql
@@ -21,10 +21,10 @@ CREATE TABLE `mediwhere`.`doctors` (
 CREATE TABLE `mediwhere`.`hospital` (
   `hospitalid` INT NOT NULL AUTO_INCREMENT,
   `serviceid` INT NOT NULL,
-  `name` VARCHAR(100) NOT NULL,
+  `hospital_name` VARCHAR(100) NOT NULL,
   `address` VARCHAR(500) NOT NULL,
   `lat` FLOAT NOT NULL,
-  `long` FLOAT NOT NULL,
+  `longh` FLOAT NOT NULL,
   `hotline` VARCHAR(15) NULL,
   `access` DOUBLE NOT NULL,
   `afford` DOUBLE NOT NULL,
@@ -116,3 +116,12 @@ ADD CONSTRAINT `hospital`
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
+INSERT INTO services(anasthesia,bar,cardio,ctscan,derma,dentmed,diabetes,endo,ent,gastro,gensurgery,hema,hepasurgery,infdisease,intcare,neon,neuro,nephro,nutridiet,obgyne,onco,opthal,ortho,pedia,plasurgery,psych,pulmo,rheuma,thoracic,uro,vitreo)
+VALUES(1,0,1,1,1,1,1,0,1,1,1,1,1,1,1,0,1,0,1,1,0,1,1,1,0,1,1,1,0,1,0),
+      (1,0,1,1,0,1,1,0,1,1,1,1,0,1,1,0,1,0,1,0,0,1,1,1,1,1,1,1,0,1,1),
+	  (1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1);
+
+INSERT INTO hospital(serviceid, hospital_name , address, lat,longh,hotline,access,afford,ambiance)
+VALUES(1, "San Juan Medical Center", "N Domingo St, San Juan, Metro Manila, Philippines", 14.606804, 121.023479, "(02) 724 3266", 5, 4.5, 4),
+      (2, "St. Martin De Porres Charity Hospital", "70 A. Bonifacio St., San Juan, Metro Manila, Philippines", 14.59911613, 121.03168309, "(072) 307 4143", 3.5, 3, 2.5),
+      (3, "Cardinal Santos Medical Center", "10 Wilson St. Greenhills West, San Juan City, Metro Manila, Philippines", 14.597604, 121.045845, "(02) 727 0001", 2, 1.5, 1);

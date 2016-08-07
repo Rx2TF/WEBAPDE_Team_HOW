c92caf91d50e806a031aa784ebb93bad40542eb1
Rx2TF <frx2tmourne@gmail.com>
2016-08-08 00:29:35 +0800

    Fixes

--- js/mediwhere-map.js
+++ js/mediwhere-map.js
@@ -26,10 +26,6 @@ function initialize() {
         border: '7px',})
     });
 
-    //loadHospital(14.606804, 121.023479, 'San Juan Medical Center', 'N Domingo St, San Juan, Metro Manila, Philippines', 5, 4.5, 4);
-    //loadHospital(14.59911613, 121.03168309, 'St. Martin de Porres Charity Hospital', '70 A. Bonifacio St., San Juan, Metro Manila, Philippines', 3.5, 3, 2.5);
-    //loadHospital(14.597604, 121.045845, 'Cardinal Santos Medical Center', '10 Wilson St. Greenhills West, San Juan City, Metro Manila, Philippines', 2, 1.5, 1);
-
     // Add markers and content for each hospital in hospitals[]
     hospitals.forEach(function(hospital) {
         console.log(hospital);
@@ -84,19 +80,6 @@ function loadMap() {
     var loadScript = document.createElement("script");
     loadScript.src = "http://maps.googleapis.com/maps/api/js?key=AIzaSyDd4GzSji1O65dMpVDE9CbqADLSYfqErt0&callback=initialize";
     document.body.appendChild(loadScript);
-}
-
-function loadHospital(latitude, longitude, hName, addrss, access, afford, ambian) {
-    var newHospital = {
-        lat: latitude,
-        lng: longitude,
-        name: hName,
-        address: addrss,
-        accessibility: access,
-        affordability: afford,
-        ambiance: ambian
-    };
-    hospitals.push(newHospital);
 }
 
 function numberToStar(rating){

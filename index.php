40554ac9aefe5bd3b81ed4abe94bcb8a15a36178
Viteszeg <rofi_santos@dlsu.edu.ph>
2016-08-07 22:14:13 +0800

    fixed php

--- index.php
+++ index.php
@@ -73,11 +73,11 @@
             if ($conn->connect_error) {
                 die("Connection failed: " . $conn->connect_error);
             } 
-            echo "Connected successfully";
+            //echo "Connected successfully";
             
-             $sql = mysqli_query("SELECT hospitalid, serviceid, name, hotline, access, afford, ambiance FROM hospital");
+            $sql = mysqli_query("SELECT hospitalid, serviceid, name, hotline, access, afford, ambiance FROM hospital");
 			$rows = array();
-			while($r = mysqli_fetch_assoc($sth)) {
+			while($r = mysqli_fetch_assoc($sql)) {
 			$rows[] = $r;
 			}
 			print json_encode($rows);
